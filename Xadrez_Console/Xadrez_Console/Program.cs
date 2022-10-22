d2c5fb5ce468e1c35bc7f967e450c2beeb0f54bd
Marcos Takezo Otta <Takezootta02@gmail.com>
2022-10-21 22:21:16 -0300

    Fazendo os primeiros testes de movimentação

--- Xadrez_Console/Xadrez_Console/Program.cs
+++ Xadrez_Console/Xadrez_Console/Program.cs
@@ -9,19 +9,24 @@ namespace Xadrez_Console
         {
             try
             {
-                Tabuleiro tab = new Tabuleiro(8, 8);
+                PartidaDeXadrez partida = new PartidaDeXadrez();
 
-                tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-                tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-                tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 2));
+                while (!partida.Terminada)
+                {
+                    Console.Clear();
+                    Tela.ImprimirTabuleiro(partida.Tab);
 
-                tab.ColocarPeca(new Torre(tab, Cor.Branca), new Posicao(2, 4));
-                tab.ColocarPeca(new Torre(tab, Cor.Branca), new Posicao(2, 1));
-                tab.ColocarPeca(new Rei(tab, Cor.Branca), new Posicao(2, 5));
+                    Console.WriteLine();
 
-                Tela.ImprimirTabuleiro(tab);
+                    Console.Write("Origem: ");
+                    Posicao origem = Tela.LerPosicaoXadrez().ToPosicao();
+                    Console.Write("Destino: ");
+                    Posicao destino = Tela.LerPosicaoXadrez().ToPosicao();
+
+                    partida.ExecutarMovimento(origem, destino);
+                }
             }
-            catch(TabuleiroException e)
+            catch (TabuleiroException e)
             {
                 Console.WriteLine(e.Message);
             }
--- Xadrez_Console/Xadrez_Console/Tela.cs
+++ Xadrez_Console/Xadrez_Console/Tela.cs
@@ -1,4 +1,5 @@
 ﻿using tabuleiro;
+using xadrez;
 
 namespace Xadrez_Console
 {
@@ -24,6 +25,14 @@ namespace Xadrez_Console
                 Console.WriteLine();
             }
             Console.WriteLine("  a b c d e f g h");
+        }
+
+        public static PosicaoXadrez LerPosicaoXadrez()
+        {
+            string s = Console.ReadLine();
+            char coluna = s[0];
+            int linha = int.Parse($"{s[1]}");
+            return new PosicaoXadrez(coluna, linha);
         }
 
         public static void ImprimirPeca(Peca peca)
